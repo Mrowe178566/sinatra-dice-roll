1591891cadf680f780b6516cd60024e844e7d214
Maia Rowe <mrowe7000@icloud.com>
2025-02-04 08:21:16 +0000

    dice and erb updates

--- dice.rb
+++ dice.rb
@@ -17,6 +17,8 @@ get("/") do
     <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
     <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
     <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
+    <li><a href=\"/dice/100/6\">Roll one hundred 6-sided dice</a></li>
+
   </ul>
   "
   erb(:elephant)
@@ -45,15 +47,15 @@ get("/dice/2/10") do
 	second_die = rand(1..10)
   sum = first_die + second_die
 	
-	outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
 	erb(:two_ten, { :layout => :wrapper })
 end
 
 get("/dice/1/20") do
-  die = rand(1..20)
+  @die = rand(1..20)
 	
-	outcome = "You rolled a #{die}.
+	@outcome = "You rolled a #{@die}.
   "
 	erb(:one_twenty, { :layout => :wrapper })
 	
@@ -66,7 +68,20 @@ get("/dice/5/4") do
   fourth_die = rand(1..4)
   sum = first_die + second_die + third_die + fourth_die
 	
-	outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
+	@outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
 	
   erb(:five_four, { :layout => :wrapper })
+end
+
+
+get("/dice/100/6") do
+  @rolls = []    # Create a blank array
+
+  100.times do    # 100 times...
+    die = rand(1..6)    # Generate a random number
+
+    @rolls.push(die)    # Add the random number to the array 
+  end
+
+  erb(:one_hundred_six, { :layout => :wrapper })
 end
